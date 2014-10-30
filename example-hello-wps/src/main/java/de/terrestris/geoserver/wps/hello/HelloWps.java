12f8425f6cd64e5e6b410cb9999b4b598a8fd565
Marc Jansen <jansen@terrestris.de>
2014-10-30 18:00:33 +0100

    A basically compiling example.

--- example-hello-wps/src/main/java/de/terrestris/geoserver/wps/hello/HelloWps.java
+++ example-hello-wps/src/main/java/de/terrestris/geoserver/wps/hello/HelloWps.java
@@ -3,13 +3,15 @@ package de.terrestris.geoserver.wps.hello;
 import org.geotools.process.factory.DescribeParameter;
 import org.geotools.process.factory.DescribeProcess;
 import org.geotools.process.factory.DescribeResult;
+
 import org.geoserver.wps.gs.GeoServerProcess;
 
-@DescribeProcess(title="helloWPS", description="Hello WPS Sample")
+@DescribeProcess(title = "helloWPS", description = "Hello WPS Sample")
 public class HelloWps implements GeoServerProcess {
 
-   @DescribeResult(name="result", description="output result")
-   public String execute(@DescribeParameter(name="name", description="name to return") String name) {
-        return "Hello, " + name;
-   }
+	@DescribeResult(name = "result", description = "output result")
+	public String execute(
+			@DescribeParameter(name = "name", description = "name to return") String name) {
+		return "Hello, " + name;
+	}
 }
