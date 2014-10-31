24b3278181a2dd38a7d091fff6a2e2af96b43f89
Marc Jansen <jansen@terrestris.de>
2014-10-31 12:52:05 +0100

    Add a bogus test.

---
+++ example-hello-wps/src/test/java/de/terrestris/geoserver/wps/hello/HelloWpsTest.java
@@ -0,0 +1,13 @@
+package de.terrestris.geoserver.wps.hello;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+
+public class HelloWpsTest {
+
+    @Test
+	public void testHumpty() {
+		assertTrue(1l==1.0f);
+	}
+}
